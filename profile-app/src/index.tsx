22f918ecdfbb4d9ea129d656b3299ae520c2ec33
Tarek Alawar <tralawar@gmail.com>
2022-06-19 02:14:46 -0700

    add routing

--- profile-app/src/index.tsx
+++ profile-app/src/index.tsx
@@ -1,6 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  BrowserRouter,
+} from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -9,9 +12,9 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+      <App />
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ profile-app/src/scenes/About/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+interface AboutProps {
+
+}
+
+export const About: React.FC<AboutProps> = ({}) => {
+        return (
+            <h1>About</h1>
+        );
+}
